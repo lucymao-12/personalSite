7325372f6c696e4214ab2a13d0d214efa07ba08c
lucymao-12 <lucymao12@gmail.com>
2024-03-20 23:46:37 -0400

    hopefully didn't break anything

--- my_site/src/components/projects.jsx
+++ my_site/src/components/projects.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ProjectImage from "./project_image";
 import cya from "../images/cya.png";
-import naive from "../images/nb_metrics.png";
-import bert from "../images/bert_metrics.png";
+import dcgan from "../images/dcgan.png";
+import hlg from "../images/higher_lower.png";
 import { Container, Row, Col } from "react-bootstrap";
 function Projects() {
   return (
@@ -16,7 +16,7 @@ function Projects() {
         justifyContent: "center",
       }}
     >
-      <h1 style={{paddingBottom: "3rem"}}>Projects</h1>
+      <h1 style={{ paddingBottom: "3rem" }}>Projects</h1>
       <p className="project-desc">
         Here are some of my projects! Most of these are from school, but I have
         also started on many personal projects as well that will hopefully fill
@@ -33,7 +33,7 @@ function Projects() {
           justifyContent: "center",
         }}
       >
-        <li className="project-title no-bullet">
+        <li className="project-title no-bullet proj1">
           Choose your own adventure multipage web app
           <p className="project-desc">
             This was made with node.js, express, mongodb backend. React, html,
@@ -50,33 +50,31 @@ function Projects() {
             alt="choose your own adventure web app"
           ></ProjectImage>
         </li>
-        <li className="project-title no-bullet">
+        <li className="project-title no-bullet proj2">
           <a href="https://colab.research.google.com/drive/1RmcNu-exn3mOe2kzZppeVq2_gExZwCEJ?usp=sharing">
-            Bert and Naive Bayes Exploration for NLP
+            DCGAN LINK, UPDATE NEEDED
           </a>
-          <p className="project-desc">
-            This is a school prejct I did in my applied machine learning course.
-            Took a pretrained BERT model and fine tuned it on a dataset from
-            hugging face. I also implemented a naive bayes model from scratch
-            and compared the difference between a deep learning model and a
-            traditional machine learning model.
-          </p>
-          <Container fluid className="outer-container flex-nowrap" style={{backgroundColor: "white"}}>
-            <Row className="justify-content-center align-items-center">
-              <Col md={6} className="text-center" style={{marginLeft: "0.5rem"}}>
-                <ProjectImage
-                  src={naive}
-                  alt="choose your own adventure web app"
-                ></ProjectImage>
-              </Col>
-              <Col md={6} className="text-center">
-                <ProjectImage
-                  src={bert}
-                  alt="choose your own adventure web app"
-                ></ProjectImage>
-              </Col>
-            </Row>
-          </Container>
+          <p className="project-desc">DCGAN DESC</p>
+          <ProjectImage
+            src={dcgan}
+            alt="screenshot of the final pass of training on my DCGAN"
+          ></ProjectImage>
+        </li>
+        <li className="project-title no-bullet proj3">
+          <a></a>
+          <p></p>
+          <ProjectImage
+            src={hlg}
+            alt="Screenshot from spotify higher or lower personal projects"
+          ></ProjectImage>
+        </li>
+        <li className="project-title no-bullet proj4">
+          <a></a>
+          <p></p>
+          <ProjectImage
+            src={hlg}
+            alt="Screenshot from spotify higher or lower personal projects"
+          ></ProjectImage>
         </li>
       </ul>
     </div>
