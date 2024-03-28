752da6b8b9df7b44827f6f3bbad09dcd76a42ebd
lucymao-12 <lucymao12@gmail.com>
2024-03-27 21:28:18 -0400

    add project details

--- my_site/src/components/projects.jsx
+++ my_site/src/components/projects.jsx
@@ -3,6 +3,8 @@ import ProjectImage from "./project_image";
 import cya from "../images/cya.png";
 import dcgan from "../images/dcgan.png";
 import hlg from "../images/higher_lower.png";
+import arrow from "../images/down-arrow-56.png";
+import marvel from "../images/marvel-db.png";
 import { Container, Row, Col } from "react-bootstrap";
 function Projects() {
   return (
@@ -16,8 +18,8 @@ function Projects() {
         justifyContent: "center",
       }}
     >
-      <h1 style={{ paddingBottom: "3rem", color: "#d8e3e1"  }}>Projects</h1>
-      <p className="project-desc" style={{color: "#d8e3e1"}}>
+      <h1 style={{ paddingBottom: "3rem", color: "#d8e3e1" }}>Projects</h1>
+      <p className="project-desc" style={{ color: "#d8e3e1" }}>
         Here are some of my projects! Most of these are from school, but I have
         also started on many personal projects as well that will hopefully fill
         this space one day.
@@ -27,10 +29,9 @@ function Projects() {
       <div
         className="proj-images"
         style={{
-          display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          maxHeight:"100%"
+          maxHeight: "100%",
         }}
       >
         <div className="project-title no-bullet proj1">
@@ -45,40 +46,53 @@ function Projects() {
             Inspiration is from the my love of writing and I wanted to
             incorporate some of that in my projects
           </p>
-          <img style={{
-          paddingTop: "2rem",
-          paddingBottom: "2rem",
-          maxWidth: "100%",
-        }}
-            src={cya}
-            alt="choose your own adventure web app"
-          ></img>
+          <ProjectImage src={cya} alt="Choose your own advent"></ProjectImage>
+          <img src={arrow} alt="down arrow" className="down-arrow"></img>
         </div>
         <div className="project-title no-bullet proj2">
-          <a href="https://colab.research.google.com/drive/1RmcNu-exn3mOe2kzZppeVq2_gExZwCEJ?usp=sharing">
-            DCGAN LINK, UPDATE NEEDED
+          <a href="https://colab.research.google.com/drive/1Ng6VGvuvdf8DkaEa_3UJY28uJKozqVyi?usp=sharing">
+            DCGAN Paper Reproduction
           </a>
-          <p className="project-desc">DCGAN DESC</p>
-          <ProjectImage 
+          <p className="project-desc">
+            Showed the effects of walking through latent space, vector
+            arithmetic with latent vectors, and the visualized the learned
+            filters of the model.
+          </p>
+          <ProjectImage
             src={dcgan}
             alt="screenshot of the final pass of training on my DCGAN"
           ></ProjectImage>
+          <img src={arrow} alt="down arrow" className="down-arrow"></img>
         </div>
         <div className="project-title no-bullet proj3">
-          <a></a>
-          <p></p>
-          <ProjectImage 
-            src={hlg}
-            alt="Screenshot from spotify higher or lower personal projects"
-          ></ProjectImage>
-        </div>
-        <div className="project-title no-bullet proj4">
-          <a></a>
-          <p></p>
+          <a href="https://github.com/lucymao-12/higher-lower">
+            Spotify Higher or Lower
+          </a>
+          <p className="project-desc">
+            Used Node, React, Spotify API to recreate the Google search higher
+            or lower game.
+          </p>
           <ProjectImage
             src={hlg}
             alt="Screenshot from spotify higher or lower personal projects"
           ></ProjectImage>
+          <img src={arrow} alt="down arrow" className="down-arrow"></img>
+        </div>
+        <div className="project-title no-bullet proj4">
+          <a href="https://www.youtube.com/watch?v=O6SQ9Y4FxXY">
+            Databse Visualization
+          </a>
+          <p className="project-desc">
+            This was actually one of this first projects I ever tackled in my CS
+            career. It touched on software design, OOP principles, Java and the
+            JavaFX framework. The database interaction allowed me understand
+            simple querying as well.
+          </p>
+          <ProjectImage
+            src={marvel}
+            alt="Screen shot from marvel database visualization software"
+          ></ProjectImage>
+          <img src={arrow} alt="down arrow" className="down-arrow"></img>
         </div>
       </div>
     </div>
